5796684e929206d1064192d87a9bd318841c06a8
fabian torres <frtt07@gmail.com>
2025-09-19 17:54:26 -0500

    header

---
+++ model/player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor(id, nickname, country, balance, position, properties, inJail, jailTurns) {
+        this.id = id;
+        this.nickname = nickname;
+        this.country = country;
+        this.balance = balance;
+        this.position = position;
+        this.properties = properties;
+        this.inJail = inJail;
+        this.jailTurns = jailTurns
+    }
+    
+}
