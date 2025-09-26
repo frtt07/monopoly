29529c87445c92c99ece5269e0c830fff51a30e9
simón-2112 <simonisaza2112@gmail.com>
2025-09-25 19:17:50 -0500

    cambios boton Iniciar Juego

--- scripts/monopoly.js
+++ scripts/monopoly.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let btnCargarCasillas = document.getElementById("btnCargarCasillas");
 
     btnCargarCasillas.addEventListener("click", function () {
-
+        document.getElementById("btnCargarCasillas").classList.add("d-none");
         let tablero = document.getElementById("tablero");
 
         fetch("http://127.0.0.1:5000/board")
